edabe127abfd5f04057870f2f91ff43e4a2bc634
sangwoo85 <51939253+sangwoo85@users.noreply.github.com>
2023-04-18 00:14:27 +0900

    Add files via upload

---
+++ MONTH1 ( 2023-04-01 ~ 2023-04-30 )/18일차/sangwoo.java
@@ -0,0 +1,48 @@
+package examplenote;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ *
+ * url : https://school.programmers.co.kr/learn/courses/30/lessons/12939
+ *
+ * 최댓값과 최솟값
+ * 문제 설명
+ * 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+ * 예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+ *
+ * 제한 조건
+ * s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+ * 입출력 예
+ *
+ * */
+public class sangwoo {
+
+    public static void main(String[] args) {
+
+        String s = "1 2 3 4";
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s){
+
+        String result = "";
+
+
+        String[] arr = s.split(" ");
+        List<Integer> arrList = new ArrayList<Integer>();
+
+        for(String x : arr){
+            arrList.add(Integer.parseInt(x));
+        }
+
+        Collections.sort(arrList);
+
+        result = arrList.get(0)+" "+arrList.get(arrList.size()-1);
+
+        return result;
+    }
+}
