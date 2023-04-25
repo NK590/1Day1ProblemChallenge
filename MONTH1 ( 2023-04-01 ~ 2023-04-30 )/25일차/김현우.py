56a2cd9dcc7d01f9df03e7fbd0a2f6542ace4dfb
NK <96505696+NK590@users.noreply.github.com>
2023-04-25 21:31:10 +0900

    Add files via upload

---
+++ MONTH1 ( 2023-04-01 ~ 2023-04-30 )/25일차/김현우.py
@@ -0,0 +1,57 @@
+import sys
+sys.setrecursionlimit(100001)
+
+input = sys.stdin.readline
+
+# BOJ11280 - 2-SAT - 3 (P4)
+
+def dfs(node, visited, stack):
+    visited[node] = True
+    for ne in graph[node]:
+        if not visited[ne]:
+            dfs(ne, visited, stack)
+    stack.append(node)
+
+def reverse_dfs(node, visited, stack):
+    visited[node] = True
+    stack.append(node)
+    indegrees[node] = indegree
+    for ne in reverse_graph[node]:
+        if not visited[ne]:
+            reverse_dfs(ne, visited, stack)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(2*n + 1)]
+reverse_graph = [[] for _ in range(2*n + 1)]
+for __ in range(m):
+    a, b = map(int, input().split())
+    graph[-a].append(b)
+    graph[-b].append(a)
+    reverse_graph[b].append(-a)
+    reverse_graph[a].append(-b)
+
+stack = []
+result = []
+
+visited = [False for _ in range(2*n + 1)]
+for i in range(1, 2*n + 1):
+    if not visited[i]:
+        dfs(i, visited, stack)
+
+indegree = 0
+indegrees = [-1 for _ in range(2*n + 1)]
+visited = [False for _ in range(2*n + 1)]
+while stack:
+    scc = []
+    node = stack.pop()
+    if not visited[node]:
+        indegree += 1
+        reverse_dfs(node, visited, scc)
+        result.append(sorted(scc))
+
+for i in range(1, n+1):
+    if indegrees[i] == indegrees[-i]:
+        print(0)
+        break
+else:
+    print(1)
