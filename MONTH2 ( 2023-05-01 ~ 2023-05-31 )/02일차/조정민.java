746869d94f474245686c9cca056364bcc74b1e5a
mnj190 <68101263+mnj190@users.noreply.github.com>
2023-05-02 10:32:08 +0900

    Update 조정민.java

--- MONTH2 ( 2023-05-01 ~ 2023-05-31 )/02일차/조정민.java
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/02일차/조정민.java
@@ -1,36 +1,89 @@
-// «¡∑Œ±◊∑°∏”Ω∫ - «’Ω¬ ≈√Ω√ ø‰±›
+// Î∞±Ï§Ä 4963 - ÏÑ¨Ïùò Í∞úÏàò
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Solution {
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = Integer.MAX_VALUE;
-        int[][] dist = new int[n+1][n+1];
+public class Main {
 
-        for(int i = 0; i < dist.length; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE / 10);
-            dist[i][i] = 0;
-        }
+	static int[] X = {0, 1, 0, -1, 1, -1, 1, -1};
+	static int[] Y = {1, 0, -1, 0, 1, -1, -1, 1};
 
-        for(int[] fare : fares) {
-            dist[fare[0]][fare[1]] = fare[2];
-            dist[fare[1]][fare[0]] = fare[2];
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i = 0; i <= n; i++) {
-            for(int j = 0; j <= n; j++) {
-                for(int k = 0; k <= n; k++) {
-                    if(dist[j][k] > dist[j][i] + dist[i][k])
-                        dist[j][k] = dist[j][i] + dist[i][k];
-                }
-            }
-        }
+		while(true) {
+			String[] str = br.readLine().split(" ");
+			int w = Integer.valueOf(str[0]);
+			int h = Integer.valueOf(str[1]);
+			int cnt = 0;
 
-        for(int i = 1; i <= n; i++) {
-            answer = Math.min(answer, dist[i][s] + dist[i][a] + dist[i][b]);
-            // System.out.println(answer + " : " + i);
-        }
+			if(w == 0 && h == 0)
+				break;
 
-        return answer;
-    }
+			String[][] map = new String[h][w];
+			boolean[][] visited = new boolean[h][w];
+
+			for(int i = 0; i < h; i++) {
+				map[i] = br.readLine().split(" ");
+				Arrays.fill(visited[i], false);
+			}
+
+			for(int i = 0; i < h; i ++) {
+				for(int j = 0; j < w; j++) {
+					if(visited[i][j])
+						continue;
+
+					bfs(map, visited, new int[] {i, j});
+
+					if(map[i][j].equals("1"))
+						cnt++;
+				}
+			}
+
+			bw.append(cnt + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+
+	public static void bfs(String[][] map, boolean[][] visited, int[] arr) {
+		Queue<int[]> queue = new LinkedList<>();
+		String s = map[arr[0]][arr[1]];
+		queue.add(arr);
+
+		while(!queue.isEmpty()) {
+			int[] temp = queue.poll();
+			int x = temp[0];
+			int y = temp[1];
+
+			if(visited[x][y])
+				continue;
+
+			visited[x][y] = true;
+
+			for(int i = 0; i < 8; i++) {
+				int x2 = x + X[i];
+				int y2 = y + Y[i];
+
+				if(x2 < 0 || x2 >= map.length || y2 < 0 || y2 >= map[0].length)
+					continue;
+
+				if(visited[x2][y2])
+					continue;
+
+				if(!map[x2][y2].equals(s))
+					continue;
+
+				queue.add(new int[] {x2, y2});
+			}
+		}
+	}
 }
