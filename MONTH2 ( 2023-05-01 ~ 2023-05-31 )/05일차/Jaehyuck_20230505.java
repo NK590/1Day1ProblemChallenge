64d0a7a2f1786f5a2bc0cc445eb1de7052ae3ffe
JaehyuckJo <91078393+JaehyuckJo@users.noreply.github.com>
2023-05-05 11:45:17 +0900

    lol

---
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/05일차/Jaehyuck_20230505.java
@@ -0,0 +1,27 @@
+package May;
+import java.util.*;
+// 백준 silver Lv4 설탕 배달
+// https://www.acmicpc.net/problem/2839
+
+public class Jaehyuck_20230505 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
+		}
+		else if (N % 5 == 0) {
+			System.out.println(N / 5);
+		}
+		else if (N % 5 == 1 || N % 5 == 3) {
+			System.out.println((N / 5) + 1);
+		}
+		else if (N % 5 == 2 || N % 5 == 4) {
+			System.out.println((N / 5) + 2);
+		}
+		
+
+	}
+
+}
