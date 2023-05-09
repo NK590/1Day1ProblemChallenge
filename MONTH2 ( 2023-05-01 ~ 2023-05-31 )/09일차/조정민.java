0b6daad3ff55f6d68be5f886113871583a1dbf7f
mnj190 <68101263+mnj190@users.noreply.github.com>
2023-05-09 20:12:20 +0900

    Update 조정민.java

--- MONTH2 ( 2023-05-01 ~ 2023-05-31 )/09일차/조정민.java
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/09일차/조정민.java
@@ -1,87 +1,32 @@
-// 나이트의 이동
+// 백준 1715 - 카드 정렬하기
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    static int[] X = {2, 2, -2, -2, 1, -1, 1, -1};
-    static int[] Y = {1, -1, 1, -1, 2, 2, -2, -2};
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.valueOf(br.readLine());
+        int N = Integer.valueOf(br.readLine());
+        PriorityQueueInteger pq = new PriorityQueue();
+        int answer = 0;
 
-        while(n-- > 0) {
-            int l = Integer.valueOf(br.readLine());
-            String[] start = br.readLine().split(" ");
-            String[] end = br.readLine().split(" ");
-            int x = Integer.valueOf(start[0]);
-            int y = Integer.valueOf(start[1]);
-            int x2 = Integer.valueOf(end[0]);
-            int y2 = Integer.valueOf(end[1]);
-
-            boolean[][] visited = new boolean[l][l];
-
-            for(int i = 0; i < l; i++) {
-                Arrays.fill(visited[i], false);
-            }
-
-            int reuslt = bfs(visited, new int[] {x, y}, new int[] {x2, y2});
-
-            bw.append(reuslt + "\n");
+        for(int i = 0; i  N; i++) {
+            pq.add(Integer.valueOf(br.readLine()));
         }
 
-        bw.flush();
-        bw.close();
-    }
+        while(pq.size()  1) {
+            int c = pq.poll() + pq.poll();
 
-    public static int bfs(boolean[][] visited, int[] arr, int[] target) {
-        Queue<int[]> queue = new LinkedList<>();
-        int cnt = 0;
-        queue.add(arr);
-
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-
-            while(size-- > 0) {
-                int[] temp = queue.poll();
-                int x = temp[0];
-                int y = temp[1];
-
-                if(x == target[0] && y == target[1])
-                    return cnt;
-
-                if(visited[x][y])
-                    continue;
-
-                visited[x][y] = true;
-
-                for(int i = 0; i < 8; i++) {
-                    int x2 = x + X[i];
-                    int y2 = y + Y[i];
-
-                    if(x2 < 0 || x2 >= visited.length || y2 < 0 || y2 >= visited.length)
-                        continue;
-
-                    if(visited[x2][y2])
-                        continue;
-
-                    queue.add(new int[] {x2, y2});
-                }
-            }
-
-            cnt++;
+            answer += c;
+            pq.add(c);
         }
 
-        return cnt;
+        System.out.println(answer);
     }
 }
