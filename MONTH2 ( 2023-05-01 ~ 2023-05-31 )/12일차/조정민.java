589e5230be111d5c379097fdc0ff42e6d85a5c25
mnj190 <68101263+mnj190@users.noreply.github.com>
2023-05-12 20:38:18 +0900

    Update 조정민.java

--- MONTH2 ( 2023-05-01 ~ 2023-05-31 )/12일차/조정민.java
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/12일차/조정민.java
@@ -1,63 +1,50 @@
+// 백준 2294 - 동전 2
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
-    static List<List<int[]>> graph;
-    static boolean[] visited;
-    static int answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String a = br.readLine();
-        String b = br.readLine();
+		String[] str = br.readLine().split(" ");
+		int n = Integer.valueOf(str[0]);
+		int m = Integer.valueOf(str[1]);
 
-        String[] str = new String[a.length() + 1];
-        String[] str2 = new String[b.length() + 1];
+		int[] arr = new int[n];
+		int[] dp = new int[m + 1];
 
-        for(int i = 0; i < a.length(); i++) {
-            str[i + 1] = String.valueOf(a.charAt(i));
-        }
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.valueOf(br.readLine());
+		}
 
-        for(int i = 0; i < b.length(); i++) {
-            str2[i + 1] = String.valueOf(b.charAt(i));
-        }
+		Arrays.sort(arr);
 
-        int aSize = str.length;
-        int bSize = str2.length;
-        int[][] dp = new int[aSize][bSize];
-        int answer = 0;
+		Loop1: for (int i = 1; i <= m; i++) {
+			for (int x : arr) {
+				if (x > m)
+					continue Loop1;
 
-        for (int i = 0; i < aSize; i++) {
-            for (int j = 0; j < bSize; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (str[i].equals(str2[j])) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
+				if (x == i) {
+					dp[i] = 1;
+				} else if (x < i) {
+					if (dp[i - x] == 0)
+						continue;
 
-        for (int i = 0; i < aSize; i++) {
-            for (int j = 0; j < bSize; j++) {
-                answer = Math.max(answer, dp[i][j]);
-            }
-        }
+					int y = dp[i - x] + 1;
 
+					if (dp[i] == 0 || dp[i] > y) {
+						dp[i] = y;
+					}
+				}
+			}
+		}
 
-        bw.append(answer + "\n");
-        bw.flush();
-        bw.close();
-    }
-
+		System.out.println((dp[m] == 0) ? -1 : dp[m]);
+	}
 }
