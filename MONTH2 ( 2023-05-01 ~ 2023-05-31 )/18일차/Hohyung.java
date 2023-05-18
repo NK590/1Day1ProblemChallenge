557e539ce123c56c8667bd350e6915e8c47660a9
이호형 <77003178+LHH1115@users.noreply.github.com>
2023-05-18 10:31:27 +0900

    Update Hohyung.java

--- MONTH2 ( 2023-05-01 ~ 2023-05-31 )/18일차/Hohyung.java
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/18일차/Hohyung.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+// 백준 11726 2xn 타일링
+// https://www.acmicpc.net/problem/11726
+
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+        dp = new int[n + 1];
+
+        System.out.println(countTile(n));
+    }
+
+    public static int countTile(int n){
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-1]+dp[i-2]) % 10007;
+        }
+
+        return dp[n];
+    }
+}
