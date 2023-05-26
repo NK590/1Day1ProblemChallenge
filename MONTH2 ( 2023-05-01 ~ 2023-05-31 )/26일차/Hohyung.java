c1f6a3fe483949e97f66fc4774823f9989a07716
이호형 <77003178+LHH1115@users.noreply.github.com>
2023-05-26 10:12:52 +0900

    Update Hohyung.java 1427

--- MONTH2 ( 2023-05-01 ~ 2023-05-31 )/26일차/Hohyung.java
+++ MONTH2 ( 2023-05-01 ~ 2023-05-31 )/26일차/Hohyung.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+// 백준 1427 소트인사이드
+// https://www.acmicpc.net/problem/1427
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.nextLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+			    System.out.print(arr[i]);
+		  }
+    }
+}
